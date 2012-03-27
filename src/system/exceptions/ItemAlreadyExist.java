2b62db4ad6f09ca24e3d700479a7fdbeba3e7ff1
Nofar Bluestein <nofarb@gmail.com>
2012-03-27 22:28:11 +0200

    new exception type

--- src/system/exceptions/ItemAlreadyExist.java
+++ src/system/exceptions/ItemAlreadyExist.java
@@ -2,9 +2,7 @@ package system.exceptions;
 
 public class ItemAlreadyExist extends Exception{
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 4170698869206905955L;
 
 	public ItemAlreadyExist(String errMessage) {
